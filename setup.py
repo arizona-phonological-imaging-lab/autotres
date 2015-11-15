7c6343207b9f866908aac947d564815c65fe7cc8
Gus Hahn-Powell <gushahnpowell@gmail.com>
2015-11-15 00:09:41 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='autotres',
+      version='0.1',
+      description='A library for automatically detecting tongue contours in ultrasound images',
+      url='http://github.com/arizona-phonological-imaging-lab/autotres',
+      author='APIL',
+      author_email='gushahnpowell@gmail.com',
+      license='Apache',
+      packages=['a3'],
+      zip_safe=False)
