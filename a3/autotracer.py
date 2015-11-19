c66cc7e30df52775fc13eb08c085cd46356507c3
Benjamin Martin <bamartin@email.arizona.edu>
2015-11-19 13:24:23 -0700

    added names for layers

--- a3/autotracer.py
+++ a3/autotracer.py
@@ -136,7 +136,9 @@ class Autotracer(object):
             raise ShapeError(self.Xshpae,self.yshape)
 
         l_in = {
-            k:lasagne.layers.InputLayer(shape=(None,)+self.Xshape[k])
+            k:lasagne.layers.InputLayer(
+                shape=(None,)+self.Xshape[k],
+                name="Input %s"%(k,))
             for k in self.predictors}
 
         self.layer_in = [l_in[k] for k in self.predictors]
@@ -146,23 +148,26 @@ class Autotracer(object):
                     l_in[k],
                     num_units = layer_size,
                     nonlinearity =lasagne.nonlinearities.rectify,
-                    W = lasagne.init.GlorotUniform())
+                    W = lasagne.init.GlorotUniform(),
+                    name="Filter %s"%(k,))
             for k in self.predictors]
-        l_hidden1 = lasagne.layers.ConcatLayer(input_filters)
-        l_hidden1_d = lasagne.layers.DropoutLayer(l_hidden1, p=.5)
-
+        l_hidden1 = lasagne.layers.ConcatLayer(input_filters,name="Concat")
+        l_hidden1_d = lasagne.layers.DropoutLayer(l_hidden1, p=.5, name="Dropout")
+        
 
         l_hidden2 = lasagne.layers.DenseLayer(
             l_hidden1_d,
             num_units = layer_size,
             nonlinearity = lasagne.nonlinearities.rectify,
-            W = lasagne.init.GlorotUniform())
-        l_hidden2_d = lasagne.layers.DropoutLayer(l_hidden2, p=.5)
+            W = lasagne.init.GlorotUniform(),
+            name="Hidden")
+        l_hidden2_d = lasagne.layers.DropoutLayer(l_hidden2, p=.5,name="Dropout")
         l_hidden3 = lasagne.layers.DenseLayer(
             l_hidden2_d,
             num_units = self.yshape[0],
             nonlinearity = lasagne.nonlinearities.rectify,
-            W = lasagne.init.GlorotUniform())
+            W = lasagne.init.GlorotUniform(),
+            name="Output")
 
         self.layer_out = l_hidden3
 
