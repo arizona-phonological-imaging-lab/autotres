b330bd6576513abba59c2de93dbaac88ffd6e141
Gus Hahn-Powell <gushahnpowell@gmail.com>
2015-11-28 23:46:39 -0700

    Cleanup smoothing methods

--- a3/smoothing.py
+++ a3/smoothing.py
@@ -1,20 +1,60 @@
 from scipy.signal import savgol_filter
+#from scipy.signal import argrelextrema
 from .translate import *
+import numpy as np
 
+class TraceCorrection(object):
+    """
+    Methods for smoothing traces
+    """
+    @classmethod
+    def trim_by_min(cls, trace):
+        """
+        returns a new Trace with values preceding either min set to zero
+        """
+        early_min, late_min = cls.get_minima(trace.coordinates)
+        nc = trace.coordinates[:]
+        empty_point = (0,0)
+        nc[:early_min] = early_min * [empty_point]
+        nc[late_min:] = [empty_point for i in range(late_min, len(nc))]
+        return Trace(image=trace.image, tracer="trimmed_min", coordinates=nc, metadata=trace.metadata)
 
-def threshold_trace(t, anterior_mean, body_mean, front_mean):
-    coords = t.coordinates
-    edge_length = int(len(coords) / 4 * 1.5)
-    rear = coords[:edge_length]
-    body = coords[edge_length:-1 * edge_length]
-    front = coords[-1 * edge_length:]
-    # print(t)
-    # print("edge length: {}".format(edge_length))
-    # print("rear: {0}".format(len(rear)))
-    # print("body: {0}".format(len(body)))
-    # print("front: {0}".format(len(front)))
-    # print("all point: {}".format(len(rear) + len(body) + len(front)))
-    def threshold_coordinates(coords, threshold):
+    @classmethod
+    def get_minima(cls, coords):
+        """
+        Divide a Trace in 2 and find the position of the min y value for each section
+        """
+        x,y = zip(*coords)
+        # switch sign
+        y = -np.array(y)
+        midpoint = int(len(x)/2)
+        return (np.argmin(y[:midpoint]), midpoint + np.argmin(y[midpoint:]))
+
+    @classmethod
+    def sg_filtering(cls, t, window=None, order=9):
+        """
+        Applies Savitsky-Golay filtering to a Trace's y values
+        """
+        if window:
+            if window % 2 == 0:
+                raise WindowException("Window size must be odd")
+            if order >= window:
+                raise WindowException("Order must be less than window size")
+        x,y = zip(*t.coordinates)
+        # window must be odd
+        window_size = window if window else max([i for i in range(0, len(y)) if i % 2 != 0])
+        y_hat = savgol_filter(y, window_size, order)
+        if len(y_hat) != 32 and len(y) == 32:
+            print(t.image)
+        # zip together the smoothed y values with the old x values
+        new_coords = list(zip(x,y_hat))
+        tracer = "svagol_filter-w={}-order={}".format(window_size, order)
+        return Trace(image=t.image, tracer=tracer, coordinates=new_coords, metadata=t.metadata)
+
+    @classmethod
+    def threshold_coordinates(cls, coords, threshold):
+        """
+        """
         # unzip the coordinates
         x,y = zip(*coords)
         # convert to lists
@@ -39,42 +79,27 @@ def threshold_trace(t, anterior_mean, body_mean, front_mean):
             diff *= sign
             # thresholded y value
             adjusted_y = current_y + diff
-            #print("adjusted_y: {}".format(adjusted_y))
             y[i+1] = adjusted_y
         return list(zip(x,y))
-    thresholded_rear = threshold_coordinates(list(reversed(rear)), anterior_mean)
-    thresholded_body = threshold_coordinates(body, body_mean)
-    thresholded_front = threshold_coordinates(front, front_mean)
-    thresholded_coords = thresholded_front + thresholded_body + thresholded_rear
-    tracer = "thresholded-rm={0}-bm={1}-fm={2}".format(anterior_mean, body_mean, front_mean)
-    return Trace(image=t.image, tracer=tracer, coordinates=thresholded_coords, metadata=t.metadata)
 
-def trim_trace(t, left_k=2, right_k=2):
-    """
-    trim k points from either side of a Trace's non-empty coordinates
-    """
-    trimmed_nonempty = t.nonempty[0+left_k:right_k*-1]
-    tracer = "trimmed_left-k={0}_right-k{1}=".format(left_k, right_k)
-    return Trace(image=t.image, tracer=tracer, coordinates=trimmed_nonempty, metadata=t.metadata)
+    @classmethod
+    def threshold_trace(cls, t, anterior_mean, body_mean, front_mean):
+        """
+        """
+        coords = t.coordinates
+        edge_length = int(len(coords) / 4 * 1.5)
+        rear = coords[:edge_length]
+        body = coords[edge_length:-1 * edge_length]
+        front = coords[-1 * edge_length:]
+        # remove rear mininimum
+        reversed_rear = list(reversed(rear))
+        thresholded_rear = reversed(cls.threshold_coordinates(reversed_rear, anterior_mean))
+        thresholded_body = cls.threshold_coordinates(body, body_mean)
+        thresholded_front = cls.threshold_coordinates(front, front_mean)
+        thresholded_coords = cls.thresholded_front + thresholded_body + thresholded_rear
+        tracer = "thresholded-rm={0}-bm={1}-fm={2}".format(anterior_mean, body_mean, front_mean)
+        return Trace(image=t.image, tracer=tracer, coordinates=thresholded_coords, metadata=t.metadata)
 
-def smooth_trace(t, window=None, order=3):
-    """
-    Applies savgol filtering to a trace's y values
-    """
-    if window:
-        if window % 2 == 0:
-            raise WindowException("Window size must be odd")
-        if order >= window:
-            raise WindowException("Order must be less than window size")
-    # only correct the non-empty points
-    x,y = zip(*t.nonempty)
-    # window must be odd
-    window_size = window if window else len(y) - 1
-    y_hat = savgol_filter(y, window_size, order)
-    # zip together the smoothed y values with the old x values
-    new_coords = list(zip(x,y_hat))
-    tracer = "svagol_filter-w={}-order={}".format(window, order)
-    return Trace(image=t.image, tracer=tracer, coordinates=new_coords, metadata=t.metadata)
 
 class WindowException(Exception):
     pass
